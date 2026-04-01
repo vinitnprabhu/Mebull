8f98ca2ad2a819ad5cc83ca67af23737766b4de0
Vinit Prabhu <vinitprabhu@Vinits-Laptop.local>
2026-04-01 15:43:22 -0400

    implemented header

---
+++ components/Navitems.tsx
@@ -0,0 +1,33 @@
+'use client'
+import Link from "next/link";
+import { NAV_ITEMS } from "@/lib/constants";
+import { usePathname } from "next/navigation";
+
+const NavItems = () => {
+
+    const pathname = usePathname();
+
+    const isActive = (path : string) => { 
+        if(path === '/') return pathname === '/';
+        return pathname.startsWith(path);
+    }
+
+    return (
+        <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
+            {NAV_ITEMS.map(({ href, title }) => (
+                <li key={href}>
+                    <Link
+                        href={href}
+                        className={`hover:text-green-500 transition-colors ${
+                            isActive(href) ? "text-gray-100" : ""
+                        }`}
+                    >
+                        {title}
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default NavItems;
