15ca3a50c87f0f1ba7f0e6bb67033a97599ebccf
Vinit Prabhu <vinitprabhu@Vinits-Laptop.local>
2026-04-22 12:33:58 -0400

    implement individual stock page

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,6 @@
 export const NAV_ITEMS = [
     { href: '/', label: 'Dashboard' },
     { href: '/search', label: 'Search' },
-    { href: '/watchlist', label: 'Watchlist'}
 ];
 
 // Sign-up form select options
